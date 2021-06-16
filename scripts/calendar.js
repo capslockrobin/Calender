4eba378d53b1c153bae79421239e83b79c0da89a
capslockrobin <71007168+capslockrobin@users.noreply.github.com>
2021-06-16 16:54:13 +0200

    fix dynamic css on different month on class day

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -60,12 +60,17 @@ function loadDays() {
     const totalDays = daysInChosenMonth(month, year);
     const dayofweek = mockDate.getDay(); // find where to start calendar day of week
 
-    for (let i = 0; i <= dayofweek; i++) {
+    console.log(dayofweek);
+    if(dayofweek < 6)
+    {
+        for (let i = 0; i <= dayofweek; i++) {
         let dayDiv = document.createElement("div");
         dayDiv.classList.add("empty");
         dayDiv.classList.add("day");
         document.getElementById("calDays").appendChild(dayDiv);
+        }
     }
+ 
 
     for (let i = 0; i < totalDays; i++) {
         const dayNumber = i + 1;
@@ -84,12 +89,26 @@ function loadDays() {
         dates.push(dateId);
         dayDiv.id = dateIdPartString;
         dayDiv.className = "day";
+        if(dayofweek == 6) {
+            dayDiv.setAttribute("style", "height: 16%;");
+        } else if(dayofweek >3) {
+            dayDiv.setAttribute("style", "height: 13%;");
+        } else {
+            dayDiv.setAttribute("style", "height: 16%;");
+        }
         dayDiv.classList.add("calendar-day");
         daySpan.className = "daySpan";
         daySpan.innerHTML = dayNumber;
 
         dayDiv.appendChild(daySpan);
 
+        document.getElementById("calDays").appendChild(dayDiv);
+    }
+
+     for (let i = 0; i <= dayofweek - 1; i++) {
+        let dayDiv = document.createElement("div");
+        dayDiv.classList.add("empty");
+        dayDiv.classList.add("day");
         document.getElementById("calDays").appendChild(dayDiv);
     }
 
